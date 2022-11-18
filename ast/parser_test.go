9a106e8ea557eb7326ea124391a01eb3411c2376
awitas <awitas@vindicotech.com>
2022-11-18 07:06:49 -0800

    added parser, stringifier

---
+++ ast/parser_test.go
@@ -0,0 +1,61 @@
+package ast
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"go/ast"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestParseExpr(t *testing.T) {
+
+	rec := struct {
+		ID   int
+		Name string
+		Sub  []*struct {
+			Active bool
+			Ts     *time.Time
+		}
+	}{}
+
+	var testCases = []struct {
+		description string
+		expr        string
+		expect      string
+	}{
+		{
+			description: "compile inline type",
+			expr:        fmt.Sprintf(`type Foo %v`, reflect.TypeOf(rec).String()),
+			expect: `type Foo struct {
+	ID   int
+	Name string
+	Sub  *struct {
+		Active bool
+		Ts     *time.Time
+	}
+}`,
+		},
+	}
+
+	for _, testCase := range testCases {
+		root, err := ParseExpr(testCase.expr)
+		if !assert.Nil(t, err, testCase.description) {
+			continue
+		}
+		fn, ok := root.(*ast.FuncLit)
+		assert.True(t, ok)
+		if !assert.Nil(t, err, testCase.description) {
+			continue
+		}
+		//toolbox.Dump(root)
+		actual, err := StringifyExpr(fn.Body.List[0])
+		if !assert.Nil(t, err, testCase.description) {
+			continue
+		}
+		assert.EqualValues(t, testCase.expect, strings.TrimSpace(actual), testCase.description)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,10 @@ require github.com/stretchr/testify v1.8.1
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-errors/errors v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/viant/toolbox v0.34.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
